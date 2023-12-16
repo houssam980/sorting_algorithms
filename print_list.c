b3f67ac9685afcea43dd28da6deb54f9eb8cf9ee
houssam980 <houssam.ghmari1243@gmail.com>
2023-12-16 11:49:51 -0800

    fixed list and arry files

--- print_list.c
+++ print_list.c
@@ -3,22 +3,22 @@
 
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
+* print_list - Prints a list of integers
+*
+* @list: The list to be printed
+*/
+void print_list(const listint_t *list)
 {
-    size_t i;
+int i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+i = 0;
+while (list)
+{
+if (i > 0)
+printf(", ");
+printf("%d", list->n);
+++i;
+list = list->next;
+}
+printf("\n");
 }
